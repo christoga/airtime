36e69b37eca9eab55551c7b20206eb74a8cd1cc8
Andre Christoga <andrechristoga@gmail.com>
2017-01-04 01:25:18 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function fetch(name, time) {
+	'use strict';
+
+	alert('Your will arive to ' + name + ' in about ' + 0.375 * time + ' hour')
+}
+
+function count() {
+	'use strict';
+	// Inputs
+	var city = $('#city').val();
+	if (city == 'tokyo') {
+		fetch('Tokyo', 6)
+	} else if (city == 'seoul') {
+		fetch('Seoul', 5.5)
+	} else if (city == 'singapore') {
+		fetch('Singapore', 1.5)
+	} else if (city == 'angkara') {
+		fetch('Angkara', 4)
+	} else if (city == 'taipei') {
+		fetch('Taipei', 1.5)
+	}
+}
